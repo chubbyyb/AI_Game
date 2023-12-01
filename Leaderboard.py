d3e7acc41862a8ec94163f5101cce43cfd374091
chubs <keithsalhani@proton.me>
2023-12-01 20:37:27 +0000

    modified:   Leaderboard.py

--- Leaderboard.py
+++ Leaderboard.py
@@ -4,26 +4,25 @@ class Leaderboard:
 
     def __init__(self):
         with open("Leaderboard.json", "r") as leaderboard:
-            jsonFile = json.load(leaderboard)
+            self.jsonFile = json.load(leaderboard)
 
     def checkScore(self, name):
         try:
-            print(jsonFile[name])
+            print(self.jsonFile[name])
         except:
             print("User doesn't exist")
             self.createScore(name)
 
-    def createScore(self):
-        jsonFile[name] = 0
+    def createScore(self, name):
+        self.jsonFile[name] = 0
         self.saveScore()
 
     def changeScore(self, name, score):
-        jsonFile[name] = score
+        self.jsonFile[name] = score
         self.saveScore()
 
     def saveScore(self):
         with open("leaderboard.json", "w") as leaderboard:
-            json.dump(jsonFile, leaderboard, indent=2)
-
+            json.dump(self.jsonFile, leaderboard, indent=2)
 
 
