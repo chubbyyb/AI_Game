f9174dae81e0c59376dadbf73362d75a9a864e5e
chubs <keithsalhani@proton.me>
2023-12-03 18:02:56 +0000

    Added comments

--- Leaderboard.py
+++ Leaderboard.py
@@ -3,26 +3,26 @@ import json
 class Leaderboard:
 
     def __init__(self):
-        with open("Leaderboard.json", "r") as leaderboard:
-            self.jsonFile = json.load(leaderboard)
+        with open("Leaderboard.json", "r") as leaderboard: # This opens the json file with 'read' permissions
+            self.jsonFile = json.load(leaderboard) # This is the json file that stores the leaderboard
 
     def checkScore(self, name):
         try:
             print(f"Your score is: {self.jsonFile[name]}")
         except:
             print("This is your first time playing! Welcome!")
-            self.createScore(name)
+            self.createScore(name) # This creates a new score for the player
 
     def createScore(self, name):
-        self.jsonFile[name] = 0
-        self.saveScore()
+        self.jsonFile[name] = 0 # This creates a new score for the player with the new name
+        self.saveScore() # This saves the score to the json file
 
     def changeScore(self, name, score):
         self.jsonFile[name] = score
         self.saveScore()
 
     def saveScore(self):
-        with open("leaderboard.json", "w") as leaderboard:
-            json.dump(self.jsonFile, leaderboard, indent=2)
+        with open("leaderboard.json", "w") as leaderboard: # This opens the json file with 'write' permissions
+            json.dump(self.jsonFile, leaderboard, indent=2) # This saves the score to the json file
 
 
--- minigame.py
+++ minigame.py
@@ -3,7 +3,7 @@ import random
 class Minigame:
     def key_guessing_game(self):
         # Generates random number for key between 1 and 3
-        key = random.randint(1, 3)
+        key = random.randint(1, 3) 
         
         while True:
             # prompts user to guess the key
@@ -12,7 +12,7 @@ class Minigame:
             # check if guess is correct
             if guess == key:
                 print("Congratulations! You guessed the correct number! The clue has been revealed, press e.")
-                return True
+                return True # returns true if the guess is correct
             else:
                 print("Wrong number! Try again.")
             
